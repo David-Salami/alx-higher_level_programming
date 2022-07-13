0a8cecb58bb1fbf0270d542061c0fa1a95bbb47e
David-Salami <salamiidowudavid@gmail.com>
2022-07-13 08:57:16 -0700

    my correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import add_0 as ad
-ad.add.a = 1
-ad.add.b = 2
-val = ad.add(ad.add.a, ad.add.b)
-print("{} + {} = {}".format(ad.add.a, ad.add.b, val))
+"""Print the sum of 1 and 2."""
+from add_0 import add
+
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
